65d1e39289aae4c8868f9b320cfa4aeaa6c0fe8e
Taliesin Sisson <taliesins@interxion.com>
2015-05-19 14:55:38 +0100

    Add minification and cleanup tasks.

--- common/microsoft-updates.ps1
+++ common/microsoft-updates.ps1
@@ -1,12 +1,8 @@
-net stop wuauserv
+stop-service wuauserv
+&reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v EnableFeaturedSoftware /t REG_DWORD /d 1 /f
+&reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v IncludeRecommendedUpdates /t REG_DWORD /d 1 /f
 
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v EnableFeaturedSoftware /t REG_DWORD /d 1 /f
+$mu = New-Object -ComObject Microsoft.Update.ServiceManager -Strict 
+$result = $mu.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")
 
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v IncludeRecommendedUpdates /t REG_DWORD /d 1 /f
-
-echo Set ServiceManager = CreateObject("Microsoft.Update.ServiceManager") > A:\temp.vbs
-echo Set NewUpdateService = ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"") >> A:\temp.vbs
-
-cscript A:\temp.vbs
-
-net start wuauserv
+start-service wuauserv
--- common/ultradefrag.ps1
+++ common/ultradefrag.ps1
@@ -5,6 +5,6 @@ $download_path = "C:\Windows\Temp\$msi_file_name"
 $install_path = "C:\Windows\Temp\ultradefrag"
 
 (New-Object System.Net.WebClient).DownloadFile($download_url, $download_path)
-&"C:\Program Files\7-Zip\7z.exe" e -y -o"$install_path" "$download_path" *\udefrag.exe *\*.dll
+&"c:\7-zip\7z.exe" e -y -o"$install_path" "$download_path" *\udefrag.exe *\*.dll
 
 &"$install_path\udefrag.exe" --optimize --repeat $($env:SystemDrive)
