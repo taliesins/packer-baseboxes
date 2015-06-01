44f0ec26b5427147cd2d56c1dbaa6e5d86108ee9
Taliesin Sisson <taliesins@interxion.com>
2015-06-01 17:49:38 +0100

    Use the highest priority unattend to ensure that it is installed first. Ensure that all unattend files are removed.

--- common/set-unattend.ps1
+++ common/set-unattend.ps1
@@ -1,2 +1,10 @@
-cp a:\phase2-unattend.xml c:\unattend.xml
-remove-item c:\windows\panther\unattend.xml
+@('c:\unattend.xml', 'c:\windows\panther\unattend\unattend.xml', 'c:\windows\panther\unattend.xml', 'c:\windows\system32\sysprep\unattend.xml') | %{
+	if (test-path $_){
+		remove-item $_
+	}	
+}
+
+if (!(test-path c:\windows\panther\unattend)) {
+	mkdir c:\windows\panther\unattend
+}
+cp a:\phase2-unattend.xml c:\windows\panther\unattend\unattend.xml
