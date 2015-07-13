7592a8b89d2976b46c07d0677992981f19babb93
Taliesin Sisson <taliesins@interxion.com>
2015-07-13 16:28:30 +0100

    Swap between the 2 different partition types. Gen 2 needs UEFI.

---
+++ make answer iso.ps1
@@ -0,0 +1,26 @@
+﻿$isoFolder = "answer-iso"
+if (test-path $isoFolder){
+	remove-item $isoFolder -Force -Recurse
+}
+
+mkdir $isoFolder
+
+copy windows\windows-2012R2-serverdatacenter-amd64\Autounattend.xml $isoFolder\
+copy windows\windows-2012R2-serverdatacenter-amd64\sysprep-unattend.xml $isoFolder\
+copy windows\common\set-power-config.ps1 $isoFolder\
+copy windows\common\microsoft-updates.ps1 $isoFolder\
+copy windows\common\win-updates.ps1 $isoFolder\
+copy windows\common\run-sysprep.ps1 $isoFolder\
+copy windows\common\run-sysprep.cmd $isoFolder\
+
+$textFile = "$isoFolder\Autounattend.xml" 
+
+$c = Get-Content -Encoding UTF8 $textFile
+
+$c | % { $_ -replace '<!-- Start Non UEFI -->','<!-- Start Non UEFI' } | % { $_ -replace '<!-- Finish Non UEFI -->','Finish Non UEFI -->' } | % { $_ -replace '<!-- Start UEFI compatible','<!-- Start UEFI compatible -->' } | % { $_ -replace 'Finish UEFI compatible -->','<!-- Finish UEFI compatible -->' } | sc -Path $textFile
+
+& .\mkisofs.exe -r -iso-level 4 -o windows\windows-2012R2-serverdatacenter-amd64\answer.iso $isoFolder
+
+if (test-path $isoFolder){
+	remove-item $isoFolder -Force -Recurse
+}
