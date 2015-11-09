797251e472dc746968b748cfc45bef428e5bb136
Taliesin Sisson <taliesins@interxion.com>
2015-11-09 11:58:51 +0000

    Skip updates for now

--- windows/common/win-updates.ps1
+++ windows/common/win-updates.ps1
@@ -215,40 +215,5 @@ function Check-WindowsUpdates() {
     }
 }
 
-$script:ScriptName = $MyInvocation.MyCommand.ToString()
-$script:ScriptPath = $MyInvocation.MyCommand.Path
-$script:UpdateSession = New-Object -ComObject 'Microsoft.Update.Session'
-$script:UpdateSession.ClientApplicationID = 'Packer Windows Update Installer'
+EnableWinRm
 
-$proxyServerAddress = ""
-$proxyServerUsername = ""
-$proxyServerPassword = ""
-
-if ($proxyServerAddress) {
-    $script:WebProxy = New-Object -ComObject 'Microsoft.Update.WebProxy'
-    $script:WebProxyBypass = New-Object -ComObject 'Microsoft.Update.StringColl'
-    $script:WebProxyBypass.Add("*.localtest.me")
-    $script:WebProxy.AutoDetect = $false
-    $script:WebProxy.Address = $proxyServerAddress
-    if ($proxyServerUsername) {
-        $script:WebProxy.Username = $proxyServerUsername
-    }
-    if ($proxyServerPassword) {
-        $script:WebProxy.SetPassword($proxyServerPassword)
-    }
-    $script:WebProxy.BypassProxyOnLocal = $true
-    $script:WebProxy.BypassList = $script:WebProxyBypass
-    $script:UpdateSession.WebProxy = $script:WebProxy
-}
-
-$script:UpdateSearcher = $script:UpdateSession.CreateUpdateSearcher()
-$script:SearchResult = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-$script:Cycles = 0
-$script:CycleUpdateCount = 0
-
-Check-WindowsUpdates
-if ($global:MoreUpdates -eq 1) {
-    Install-WindowsUpdates
-} else {
-    Check-ContinueRestartOrEnd
-}
