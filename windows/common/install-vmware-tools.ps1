ae4250191b2cbfa732e44c9ba8ab02000c85f342
Taliesin Sisson <taliesins@interxion.com>
2015-11-09 10:01:52 +0000

    Do not need to set winrm in Autounattend.xml as its set in powershell script Force when setting the registry When installing vmware tools Attempt a windows reboot after installing vmware tools (suspicion is that we are reinstalling the vmxnet3 driver)

--- windows/common/install-vmware-tools.ps1
+++ windows/common/install-vmware-tools.ps1
@@ -12,6 +12,6 @@ if ($ENV:HttpIp){
 
 (New-Object System.Net.WebClient).DownloadFile($download_url, "c:\windows\temp\$iso_name")
 &"c:\7-zip\7z.exe" x "c:\windows\temp\$iso_name" -oc:\windows\temp\vmware -aoa | Out-Host
-&"c:\windows\temp\vmware\setup.exe" /S /v`"/qn REBOOT=R ADDLOCAL=ALL`" | Out-Host
+&"c:\windows\temp\vmware\setup.exe" /S /v`"/qn REBOOT=ReallySuppress ADDLOCAL=ALL`" | Out-Host
 
 exit 0
