d003f6931bb8a4f26fe4ff673ed6c0d3a73d9a7f
Taliesin Sisson <taliesins@interxion.com>
2015-11-09 11:57:14 +0000

    Specify the options to install (exclude VMXNET3 and PVSCSI drivers at windows setup has installed them)

--- windows/common/install-vmware-tools.ps1
+++ windows/common/install-vmware-tools.ps1
@@ -12,6 +12,6 @@ if ($ENV:HttpIp){
 
 (New-Object System.Net.WebClient).DownloadFile($download_url, "c:\windows\temp\$iso_name")
 &"c:\7-zip\7z.exe" x "c:\windows\temp\$iso_name" -oc:\windows\temp\vmware -aoa | Out-Host
-&"c:\windows\temp\vmware\setup.exe" /S /v`"/qn REBOOT=ReallySuppress ADDLOCAL=ALL`" | Out-Host
+&"c:\windows\temp\vmware\setup.exe" /S /v`"/qn REBOOT=ReallySuppress ADDLOCAL=Audio,Hgfs,FileIntrospection,NetowkrIntrospection,VSS,Perfmon,TrayIcon,Microsfot_x86_Dll,Common,Drivers,MemCtl,MouseUsb,SVGA,VMCI,Toolbox,Plugins,Unity`" | Out-Host
 
 exit 0
