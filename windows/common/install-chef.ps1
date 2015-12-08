89ae9bdb823917bf18c1d77c47aab93dc0931e90
Taliesin Sisson <taliesins@interxion.com>
2015-12-08 10:43:03 +0000

    Upgrade to latest version of Chef

--- windows/common/install-chef.ps1
+++ windows/common/install-chef.ps1
@@ -1,13 +1,13 @@
 $ProgressPreference="SilentlyContinue"
-$version = '12.4.0-1'
-$msi_file_name = "chef-client-$($version).msi"
+$version = '12.5.1-1'
+$msi_file_name = "chef-client-$($version)-x86.msi"
 
 if ($ENV:HttpIp){
     $httpIp = $ENV:HttpIp
     $httpPort = $ENV:HttpPort
     $download_url = "http://$($httpIp):$($httpPort)/$msi_file_name"
 } else {
-    $download_url = "http://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/x86_64/$msi_file_name"
+    $download_url = "http://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/i386/$msi_file_name"
 }
 
 (New-Object System.Net.WebClient).DownloadFile($download_url, "C:\Windows\Temp\$msi_file_name")
