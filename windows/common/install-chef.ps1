491d926a2b7c56519b59e355c6971381482acb71
Taliesin Sisson <taliesins@yahoo.com>
2016-01-19 15:23:28 +0000

    Update to latest version of chef Update parameters that are passed into Vmware installation as it was failing silently

--- windows/common/install-chef.ps1
+++ windows/common/install-chef.ps1
@@ -1,5 +1,5 @@
 $ProgressPreference="SilentlyContinue"
-$version = '12.5.1-1'
+$version = '12.6.0-1'
 $msi_file_name = "chef-client-$($version)-x86.msi"
 
 if ($ENV:HttpIp){
@@ -7,7 +7,7 @@ if ($ENV:HttpIp){
     $httpPort = $ENV:HttpPort
     $download_url = "http://$($httpIp):$($httpPort)/$msi_file_name"
 } else {
-    $download_url = "http://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/i386/$msi_file_name"
+    $download_url = "http://opscode-omnibus-packages.s3.amazonaws.com/windows/2012r2/i386/$msi_file_name"
 }
 
 (New-Object System.Net.WebClient).DownloadFile($download_url, "C:\Windows\Temp\$msi_file_name")
