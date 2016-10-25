5a30a7700580338012416af45038bdb0e7cae5b3
Taliesin Sisson <taliesins@yahoo.com>
2016-10-25 15:02:36 +0100

    Make it obvious which step of the process of cleanup we are running in case of failures

--- windows/common/cleanup.ps1
+++ windows/common/cleanup.ps1
@@ -11,10 +11,12 @@ for ([byte]$c = [char]'A'; $c -le [char]'Z'; $c++)
 	}
 }
 
+Write-Host "Removing temp folders"
 $tempfolders = @("C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Documents and Settings\*\Local Settings\temp\*", "C:\Users\*\Appdata\Local\Temp\*")
 Remove-Item $tempfolders -ErrorAction SilentlyContinue -Force -Recurse
 
 try{
+	Write-Host "Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase"
 	Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 } catch {
 	Write-Host "Unable to reset base. Should be ok if patches have been slipstreamed."
@@ -23,7 +25,10 @@ try{
 $moduleExist = Get-Module servermanager
 
 if ($moduleExist){
-	import-module servermanager
+	Write-Host 'Get-WindowsFeature | ? { $_.InstallState -eq "Available" } | Uninstall-WindowsFeature -Remove'
 
+	import-module servermanager
 	Get-WindowsFeature | ? { $_.InstallState -eq 'Available' } | Uninstall-WindowsFeature -Remove
 }
+
+exit 0
