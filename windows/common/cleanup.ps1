9a984487f927ee5c9233143426bae2d387c7c03a
Taliesin Sisson <taliesins@yahoo.com>
2016-10-25 12:15:51 +0100

    DISM fails for some slip streamed ISOs

--- windows/common/cleanup.ps1
+++ windows/common/cleanup.ps1
@@ -14,6 +14,12 @@ for ([byte]$c = [char]'A'; $c -le [char]'Z'; $c++)
 $tempfolders = @("C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Documents and Settings\*\Local Settings\temp\*", "C:\Users\*\Appdata\Local\Temp\*")
 Remove-Item $tempfolders -ErrorAction SilentlyContinue -Force -Recurse
 
+try{
+	Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
+} catch {
+	Write-Host "Unable to reset base. Should be ok if patches have been slipstreamed."
+}
+
 $moduleExist = Get-Module servermanager
 
 if ($moduleExist){
@@ -21,5 +27,3 @@ if ($moduleExist){
 
 	Get-WindowsFeature | ? { $_.InstallState -eq 'Available' } | Uninstall-WindowsFeature -Remove
 }
-
-Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
