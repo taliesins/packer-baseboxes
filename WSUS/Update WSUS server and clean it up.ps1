28fe5130f476c13a7d7a3d0ea0853dece6cddedd
Taliesin Sisson <taliesins@yahoo.com>
2016-10-26 09:12:47 +0100

    Import module for updating wsus

--- WSUS/Update WSUS server and clean it up.ps1
+++ WSUS/Update WSUS server and clean it up.ps1
@@ -1,1 +1,3 @@
+ipmo $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(".\PSWsusSpringClean.psm1")
+
 Invoke-WsusSpringClean -SynchroniseServer -DeclineItaniumUpdates -DeclineUnneededUpdates -DeclineCategoriesInclude @('Superseded','Pre-release') 
