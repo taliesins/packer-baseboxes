3ab6ba4b42b7aba4048c8d40809815b9b24ad7ea
Taliesin Sisson <taliesins@yahoo.com>
2017-01-19 16:20:50 +0000

    User who is running wsus update needs to be belong to WSUS Administrators group

--- WSUS/Update WSUS server and clean it up.ps1
+++ WSUS/Update WSUS server and clean it up.ps1
@@ -1,7 +1,18 @@
+$ErrorActionPreference = 'Stop'
+
 $CurrentPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 Push-Location $CurrentPath
 
+try {
+	$WsusServer = Get-WsusServer
+} catch {
+	Write-Error "Is this user member of WSUS Administrator group"
+	throw $_
+}
+
 Import-Module .\PSWsusSpringClean.psm1
+
+
 Invoke-WsusSpringClean -SynchroniseServer -DeclineItaniumUpdates -DeclineUnneededUpdates -DeclineCategoriesInclude @('Superseded','Pre-release') 
 
 Pop-Location
