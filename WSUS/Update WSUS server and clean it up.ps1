994ecf2ce08c64f962df01c6d01ca7baf33ec2b2
Taliesin Sisson <taliesins@yahoo.com>
2017-01-19 15:47:00 +0000

    Change to directory to include module

--- WSUS/Update WSUS server and clean it up.ps1
+++ WSUS/Update WSUS server and clean it up.ps1
@@ -1,3 +1,7 @@
-ipmo $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(".\PSWsusSpringClean.psm1")
+$CurrentPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+Push-Location $CurrentPath
 
+Import-Module .\PSWsusSpringClean.psm1
 Invoke-WsusSpringClean -SynchroniseServer -DeclineItaniumUpdates -DeclineUnneededUpdates -DeclineCategoriesInclude @('Superseded','Pre-release') 
+
+Pop-Location
