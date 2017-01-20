df664039256588b1236d784467b23cf39e6847ab
Taliesin Sisson <taliesins@yahoo.com>
2017-01-20 08:37:09 +0000

    We want to delete the slip stream folder not the iso extracted folder

--- SlipStreamWindows/Clean Windows 2012-R2.ps1
+++ SlipStreamWindows/Clean Windows 2012-R2.ps1
@@ -1,6 +1,7 @@
 $OsName = "9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9"
 $IsoExtractPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("..\$OsName")
+$OfflineMountFolder = $IsoExtractPath + "_Slipstream"
 
-if (Test-Path $IsoExtractPath){
-	Remove-Item -Recurse -Force $IsoExtractPath
+if (Test-Path $OfflineMountFolder){
+	Remove-Item -Recurse -Force $OfflineMountFolder
 } 
--- SlipStreamWindows/Clean Windows 2016.ps1
+++ SlipStreamWindows/Clean Windows 2016.ps1
@@ -1,6 +1,7 @@
 $OsName = "14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US"
 $IsoExtractPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("..\$OsName")
+$OfflineMountFolder = $IsoExtractPath + "_Slipstream"
 
-if (Test-Path $IsoExtractPath){
-	Remove-Item -Recurse -Force $IsoExtractPath
+if (Test-Path $OfflineMountFolder){
+	Remove-Item -Recurse -Force $OfflineMountFolder
 } 
