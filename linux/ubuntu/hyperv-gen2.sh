e78186ae83d51e53d1c1a92c45728bbff88ee88b
fwagner <fwagner@was.corp>
2017-07-31 15:17:50 +0200

    fixed issues with hyper-v gen 2 Ubuntu - missing Microsoft best practices packages   (with cleanup of kernels) - grup graphic error on hyperv

---
+++ linux/ubuntu/hyperv-gen2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#disable on hyperv (quit mode has problems with hyper-v graphics)
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/g' /etc/default/grub 
+update-grub
+
+# gen 2 EFI fix - see https://docs.microsoft.com/en-us/windows-server/virtualization/hyper-v/supported-ubuntu-virtual-machines-on-hyper-v
+cp -r /boot/efi/EFI/ubuntu/ /boot/efi/EFI/boot
+mv /boot/efi/EFI/boot/shimx64.efi /boot/efi/EFI/boot/bootx64.efi
+
+reboot
