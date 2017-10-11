567d1db0db6638c7aab1fb652aceff76d0c8a448
Taliesin Sisson <taliesins@yahoo.com>
2017-10-11 23:14:31 +0100

    Fix bug in Ubuntu network.sh script causes kernel upgrades to fail for Ubuntu 16.04

--- linux/ubuntu/network.sh
+++ linux/ubuntu/network.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/sh -eux
 
-# Disable udev persistent net rules
-rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
-rm -rf /dev/.udev/ /var/lib/dhcp3/*
+ubuntu_version="`lsb_release -r | awk '{print $2}'`";
+major_version="`echo $ubuntu_version | awk -F. '{print $1}'`";
+
+if [ "$major_version" -le "15" ] && [ "$ubuntu_version" != "15.10" ]; then
+  echo "Disabling automatic udev rules for network interfaces in Ubuntu"
+  # Disable automatic udev rules for network interfaces in Ubuntu,
+  # source: http://6.ptmc.org/164/
+  rm -f /etc/udev/rules.d/70-persistent-net.rules;
+  mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+  rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
+  rm -rf /dev/.udev/ /var/lib/dhcp3/* /var/lib/dhcp/*;
+fi
+
+# Adding a 2 sec delay to the interface up, to make the dhclient happy
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
 # Disable DNS reverse lookup
